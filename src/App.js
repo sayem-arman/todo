4c9beb2ec4cc60a0153a568742f88e39d117c8fb
Sayem Arman <sayem.arman@cityscapeintl.com>
2023-02-11 16:15:43 +0600

    todo

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
+import React, {useState} from 'react';
 import './App.css';
+import Header from './components/Header';
+import Form from './components/Form';
+import TodoLists from './components/TodosList';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [todos, setTodos] = useState ([]);
   return (
-    <div >
-      Hello
+    <div className='container' >
+      <div className='app-wrapper'>
+        <div>
+          <Header/>
+        </div>
+      <div>
+        <Form
+          input ={input}
+          setInput ={setInput}
+          todos = {todos}
+          setTodos = {setTodos}
+        />
+        </div>
+      </div>
+      <TodoLists todos={todos} setTodos={setTodos} />
     </div>
   );
-}
+};
 
 export default App;
